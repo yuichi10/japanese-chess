62bd7d133033fe84d9361968da543a8eed065e6c
yuichi10 <y_084743@yahoo.co.jp>
2017-02-01 16:45:51 +0900

    check user is login

--- app/src/main/java/com/example/yuichi/japanesechess/LoginActivity.java
+++ app/src/main/java/com/example/yuichi/japanesechess/LoginActivity.java
@@ -1,13 +1,58 @@
 package com.example.yuichi.japanesechess;
 
+import android.content.Context;
+import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+import com.example.yuichi.japanesechess.firebasemodel.UserModel;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
+    private final String FirebaseUser = "users";
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        Log.d("Login", getDeviceID());
+        login(getDeviceID());
+    }
+
+    private String getDeviceID() {
+        TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+        Log.d("Login", telephonyManager.getDeviceId());
+        String deviceId = telephonyManager.getDeviceId();
+        return deviceId;
+    }
+
+    private void login(String deveiceId) {
+        ValueEventListener isUserListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.d("Login Listner", "get data");
+                UserModel user = dataSnapshot.getValue(UserModel.class);
+                if (user == null)
+                {
+                    setContentView(R.layout.activity_login);
+                } else {
+                    setContentView(R.layout.room_list);
+                }
+                Log.d("Login Listner", "exist");
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        mDatabase.child(FirebaseUser).addListenerForSingleValueEvent(isUserListener);
     }
 }
---
+++ app/src/main/java/com/example/yuichi/japanesechess/firebasemodel/UserModel.java
@@ -0,0 +1,19 @@
+package com.example.yuichi.japanesechess.firebasemodel;
+
+/**
+ * Created by yuichi on 2017/01/22.
+ */
+
+public class UserModel {
+    public String username;
+    public int winNum;
+    public int loseNum;
+
+    public UserModel(){}
+    public UserModel(String username, int winNum, int loseNum)
+    {
+        this.username = username;
+        this.winNum = winNum;
+        this.loseNum = loseNum;
+    }
+}
