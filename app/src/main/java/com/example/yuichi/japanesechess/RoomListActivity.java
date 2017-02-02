0cf5615b9746e219213272a7f733e13df8a4943c
yuichi10 <y_084743@yahoo.co.jp>
2017-02-03 00:48:04 +0900

    show room list easily

--- app/src/main/java/com/example/yuichi/japanesechess/RoomListActivity.java
+++ app/src/main/java/com/example/yuichi/japanesechess/RoomListActivity.java
@@ -1,17 +1,28 @@
 package com.example.yuichi.japanesechess;
 
 import android.content.Context;
+import android.content.Entity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.yuichi.japanesechess.firebasemodel.RoomModel;
+import com.example.yuichi.japanesechess.firebasemodel.UserModel;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by yuichi on 2017/02/01.
@@ -20,6 +31,7 @@ import com.google.firebase.database.FirebaseDatabase;
 public class RoomListActivity extends AppCompatActivity {
     private DatabaseReference mDatabase;
     private SharedPreferences sharedData;
+    ListView roomListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +39,8 @@ public class RoomListActivity extends AppCompatActivity {
         sharedData = getSharedPreferences(getString(R.string.shared_data), Context.MODE_PRIVATE);
         setContentView(R.layout.room_list);
         mDatabase = FirebaseDatabase.getInstance().getReference();
+        roomListView = (ListView)findViewById(R.id.room_list_view);
+        setRoomList();
         createNewRoom();
     }
 
@@ -47,6 +61,42 @@ public class RoomListActivity extends AppCompatActivity {
                 sharedData.edit().putString(getString(R.string.shared_data_current_room), roomID);
             }
         });
+    }
+
+    private void setRoomList() {
+        final ArrayAdapter<String> arrayAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        ChildEventListener roomListListener = new ChildEventListener() {
+
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                RoomModel room = dataSnapshot.getValue(RoomModel.class);
+                arrayAdapter.add(dataSnapshot.getKey());
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        mDatabase.child(getString(R.string.firebase_rooms)).
+                addChildEventListener(roomListListener);
+        roomListView.setAdapter(arrayAdapter);
     }
 
 }
---
+++ app/src/main/java/com/example/yuichi/japanesechess/adaptermodel/RoomElementModel.java
@@ -0,0 +1,8 @@
+package com.example.yuichi.japanesechess.adaptermodel;
+
+/**
+ * Created by yuichi on 2017/02/03.
+ */
+
+public class RoomElementModel {
+}
