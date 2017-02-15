a77f127bf23ad305db2c80de5de9925bf4828dec
yuichi10 <y_084743@yahoo.co.jp>
2017-02-15 16:00:44 +0900

    add confirmation screen at game activity

--- app/src/main/java/com/example/yuichi/japanesechess/GameActivity.java
+++ app/src/main/java/com/example/yuichi/japanesechess/GameActivity.java
@@ -1,10 +1,13 @@
 package com.example.yuichi.japanesechess;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
@@ -17,6 +20,7 @@ import com.google.firebase.database.FirebaseDatabase;
 public class GameActivity extends AppCompatActivity {
     private DatabaseReference mDatabase;
     private SharedPreferences sharedData;
+    private AlertDialog.Builder mAlertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +29,59 @@ public class GameActivity extends AppCompatActivity {
         mDatabase = FirebaseDatabase.getInstance().getReference();
         sharedData = getSharedPreferences(getString(R.string.shared_data), Context.MODE_PRIVATE);
         String roomId = sharedData.getString(getString(R.string.shared_data_current_room), "");
-        TextView textView = (TextView)findViewById(R.id.textView);
+        setBackAram();
+        TextView textView = (TextView) findViewById(R.id.textView);
         textView.setText(roomId);
+    }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        removeRoom();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            // 戻るボタンの処理
+            mAlertDialog.create().show();
+            return true;
+        }
+        return false;
+    }
+
+    private void removeRoom() {
+        String roomId = sharedData.getString(getString(R.string.shared_data_current_room), "");
+        if (roomId != "") {
+            mDatabase.child(getString(R.string.firebase_rooms)).child(roomId).removeValue();
+        }
+    }
+
+    public void finishActivity() {
+        finish();
+    }
+
+
+    private void setBackAram() {
+        mAlertDialog = new AlertDialog.Builder(this);
+        mAlertDialog.setTitle("本当に終了しますか?");
+        mAlertDialog.setMessage("この画面から離れるとゲームが終了します");
+        mAlertDialog.setPositiveButton(
+                "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // OK ボタンクリック処理
+                        finishActivity();
+                    }
+                }
+        );
+        mAlertDialog.setNegativeButton(
+                "Cancel",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                }
+        );
     }
 }
